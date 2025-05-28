ce6d826bda21a8e7313056d5fcfcde6d151509b4
Ana Luiza Carlesso <alcarlesso3@gmail.com>
2025-05-28 10:33:31 -0300

    Create Veiculo.java

---
+++ Veiculo.java
@@ -0,0 +1,35 @@
+public class Veiculo {
+    private String marca;
+    private String modelo;
+    private int ano;
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void exibirDetalhes() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Ano: " + ano);
+    }
+}
